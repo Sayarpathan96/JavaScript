70f3d7f57115c4c351ba690e9824326a7b266b61
Sayarpathan96 <mustifizurp@gmail.com>
2024-05-09 15:59:21 +0530

    operater done

---
+++ oprater.js
@@ -0,0 +1,36 @@
+
+        // false can be reprsent by 0
+        // true can be represent by 1
+
+        // AND operator
+        let p =false   
+        let q = false
+        console.log(p && q)
+
+        // OR Operator
+
+        let s = true
+        let r = false
+
+        console.log(s||r)
+
+
+        // not operator
+        let t = false
+        console.log(!t)
+        
+
+
+        // ternary operator
+        
+        let age = 12;
+        let message = (age>=18)?("you can acces the website"):("You are Minor")
+        console.log(age,message)
+
+
+        let firstname = "mohit"
+        let lastname ="verma"
+        let fullname = firstname+" "+lastname
+        console.log(fullname)
+        let u = fullname.toUpperCase();
+        console.log(u)
