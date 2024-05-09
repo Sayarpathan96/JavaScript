12f231c3e0f4fbeb905f5c21f34d7941504f5388
Sayarpathan96 <mustifizurp@gmail.com>
2024-05-09 16:18:01 +0530

    all opraters

--- oprater.js
+++ oprater.js
@@ -34,3 +34,89 @@
         console.log(fullname)
         let u = fullname.toUpperCase();
         console.log(u)
+
+        // Arthemetic Operators
+
+let a = 5;
+let b = 7;
+let c = a + b;
+console.log(c);
+
+// subtraction
+let d = b - a;
+console.log(d);
+
+//multipication
+let e = 5 * 7;
+console.log(e);
+
+//division
+let f = a / b;
+console.log(f);
+
+//MODULE
+let g = b % a;
+console.log(g);
+
+
+//comparison operater
+
+let x = 10;
+let y = "hello";
+console.log("comparison", x = y);
+
+x = 10;
+y = "10";
+console.log("equal to", x == y);
+
+x = 10;
+y = "10";
+console.log("triple equal to", x === y);
+
+x = 12;
+y = 19;
+console.log("greater than equal to", x >= y);
+
+//logical operaters 
+// AND OR NOT
+let i = true;
+let j = false;
+console.log(i || j);
+console.log(!i);
+console.log(!j);
+console.log(i && j);
+console.log(j && i);
+console.log(typeof (j));
+
+let k = 5;
+k += 90  // k = k + 90
+console.log(k);
+
+// ++ increment 
+// first implement then n
+x = 2;
+console.log(x++);
+
+// first increament then implement
+x = 2;
+console.log(++x);
+
+// -- decrement
+// first implement then decrement
+x = 2;
+console.log(x--);
+
+// first decrement then implement
+x = 2;
+console.log(--x);
+
+
+//Exponentiation
+x = 23;
+console.log(x ** 2);
+
+// Ternary 
+let ag = 18;
+let userName = "pawan";
+let messag = (ag >= 18 && userName === "pawan") ? (`Your name:${userName} and Your age is:${age}`):("You are not an member");
+console.log(messag);
